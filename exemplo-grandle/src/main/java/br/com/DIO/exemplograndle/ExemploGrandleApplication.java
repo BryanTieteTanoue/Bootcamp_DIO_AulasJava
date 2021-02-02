03ec6cf8b06ec63251a8473ff09c50b8375b553b
Bryan Tiete Tanoue <bryan_tanoue@hotmail.com>
2021-02-02 12:55:57 -0300

    Commit inicial

---
+++ exemplo-grandle/src/main/java/br/com/DIO/exemplograndle/ExemploGrandleApplication.java
@@ -0,0 +1,13 @@
+package br.com.DIO.exemplograndle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGrandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGrandleApplication.class, args);
+	}
+
+}
---
+++ exemplo-grandle/src/test/java/br/com/DIO/exemplograndle/ExemploGrandleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DIO.exemplograndle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGrandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
